b4d98b9ffe396b9cfdf7db1ad346a40b9b1fd4a1
Mahmudul Haque Shawon <149573785+Shawon5030@users.noreply.github.com>
2025-08-20 11:00:35 +0600

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -124,6 +124,16 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ('aiquestdjango@gmail.com')
+EMAIL_HOST_PASSWORD = ('mlwr mcts vnlt jdtr')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
