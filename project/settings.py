867f9045acfbe8a661c8c9f7377a7e4d367b7410
Mahmudul Haque Shawon <haquemahmudul600@gmail.com>
2026-09-11 23:12:54 +0600

    done nano database

--- project/settings.py
+++ project/settings.py
@@ -80,11 +80,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        "postgresql://neondb_owner:npg_mTO4fQworn6C@ep-morning-cloud-aez0ktgh-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
